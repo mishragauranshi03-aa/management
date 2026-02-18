c2c91e7c10acbc3d8a8a0631d277f97e820bdb28
root <root@localhost.localdomain>
2026-02-18 12:06:07 +0530

    update from server

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,11 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-<<<<<<< HEAD
-  baseURL: "http://172.17.12.165:8002", 
-=======
-  baseURL: "http://127.0.0.1:8000", 
->>>>>>> 8926bb94bd63ac3fb0a05b4eab035e48520af105
+  baseURL: "http://172.17.12.165:8002",
 });
 
 // ---------- AUTH ----------
@@ -17,26 +13,11 @@ export const updateUser = (id, data) => API.put(`/auth/updateuser/${id}`, data);
 
 // ---------- TASKS ----------
 export const getAllTasks = () => API.get("/tasks/get");
-<<<<<<< HEAD
 export const getEmployeeTasks = (username) => API.get(`/tasks/user/${username}`);
 export const createTask = (data) => API.post("/tasks/create", data);
-=======
-export const getEmployeeTasks = (id) => API.get(`/tasks/user/${id}`);
-//export const getEmployeeTasks = (id) => API.get(`/tasks/get/${id}`);
-export const createTask = (data) => API.post("/tasks/create", data);
-//export const createTask = (data) => API.post("/tasks/", data); // <-- corrected
-
->>>>>>> 8926bb94bd63ac3fb0a05b4eab035e48520af105
 
 // correct and consistent routes
 export const updateTask = (id, data) => API.put(`/tasks/update/${id}`, data);
 export const deleteTask = (id) => API.delete(`/tasks/delete/${id}`);
-<<<<<<< HEAD
 
 export default API;
-=======
-//export const updateTask = (id, data) => API.put(`/tasks/${id}`, data);
-//export const deleteTask = (id) => API.delete(`/tasks/delete/${id}`);
-
-export default API;
->>>>>>> 8926bb94bd63ac3fb0a05b4eab035e48520af105
